30f10b1e1c5417ecfc2f0b11f76292e7c3a13314
Igor Shymko <i.shymko@thenewmotion.com>
2012-06-28 13:52:54 +0300

    init changed

--- init.el
+++ init.el
@@ -1,3 +1,16 @@
+
+(load "~/.emacs.d/el-files/ancane-modes.el")
+(load "~/.emacs.d/el-files/ancane-func.el")
+(load "~/.emacs.d/el-files/ancane-autocomplete.el")
+(load "~/.emacs.d/el-files/ancane-hooks.el")
+(load "~/.emacs.d/el-files/ancane-keys.el")
+(load "~/.emacs.d/el-files/ancane-magit.el")
+(load "~/.emacs.d/el-files/ancane-js2.el")
+(load "~/.emacs.d/el-files/ancane-scala.el")
+
+(set-default-font "terminus-12")
+
+
 (require 'dired-x)
 (setq-default dired-omit-files-p t) ; this is buffer-local variable
 (setq dired-omit-files
