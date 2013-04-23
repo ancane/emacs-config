bcd99dd4dc1dc2ade4322a44163486de9c9c3902
Igor Shymko <igor.shimko@gmail.com>
2013-04-23 19:31:14 +0300

    yasnippet snippets dir fix

--- custom/ancane-yas.el
+++ custom/ancane-yas.el
@@ -1,11 +1,10 @@
 (require 'yasnippet)
 
-;; Use only own snippets, do not use bundled ones
+;; Own snippets, not bundled ones
 (setq yas/snippet-dirs '("~/.emacs.d/custom/snippets"))
 (yas/global-mode 1)
 
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet-x.y.z/snippets")
 
 (define-key yas/keymap (kbd "<return>") 'yas/exit-all-snippets)
 
