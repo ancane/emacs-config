154ffe3181e5978f0fad480730769bf042dcd6d1
Igor Shymko <i.shymko@thenewmotion.com>
2013-06-24 19:55:52 +0300

    popup switch hook function now updates line numbers if linum mode is on

--- custom/ancane-nav.el
+++ custom/ancane-nav.el
@@ -6,11 +6,26 @@
   (let ((buff-name buffer-file-name))
     (if buff-name
         (with-current-buffer "*nav*"
-          (nav-jump-to-dir (file-name-directory buff-name))
-          )
-      (message "No file for current buffer" )
-      )
-    )
-  )
+          (progn
+            (nav-jump-to-dir (file-name-directory buff-name))
+            (if (boundp 'linum-mode)
+                (linum-update-current))))
+      (message "No file for current buffer" ))))
+
+;; Remove after hook pull request is accepted by popup-buffer
+(defcustom psw-after-switch-hook nil
+  "Hook runs after buffer switch")
+
+(defun psw-switch-buffer ()
+  (interactive)
+  (switch-to-buffer
+   (psw-popup-menu (psw-get-buffer-list) 'buffer-name))
+  (run-hooks 'psw-after-switch-hook))
+
+(defun psw-switch-recentf ()
+  (interactive)
+  (find-file
+   (psw-popup-menu recentf-list 'identity))
+  (run-hooks 'psw-after-switch-hook))
 
 (add-hook 'psw-after-switch-hook 'nav-jump-to-current-dir)
