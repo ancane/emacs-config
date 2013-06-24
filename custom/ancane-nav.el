3f68751fd64280ea519e266b70417a15332e1e29
Igor Shymko <i.shymko@thenewmotion.com>
2013-06-24 19:32:53 +0300

    popup buffer switch hook updates nav to point to a directory of current buffer

--- custom/ancane-nav.el
+++ custom/ancane-nav.el
@@ -1,1 +1,16 @@
 (require 'nav)
+
+(defun nav-jump-to-current-dir ()
+  (interactive)
+
+  (let ((buff-name buffer-file-name))
+    (if buff-name
+        (with-current-buffer "*nav*"
+          (nav-jump-to-dir (file-name-directory buff-name))
+          )
+      (message "No file for current buffer" )
+      )
+    )
+  )
+
+(add-hook 'psw-after-switch-hook 'nav-jump-to-current-dir)
